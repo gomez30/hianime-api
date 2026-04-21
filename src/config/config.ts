8b8d67d43ef5a056917c407b589c4ea3556bbcf3
gomez30 <richardgomez3096@gmail.com>
2026-04-21 19:18:28 +0600

    Use environment variable for port configuration

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,7 @@ const config = {
   baseurl: 'https://aniwatchtv.to',
   baseurl2: 'https://aniwatchtv.to',
   origin: '*',
-  port: 5000,
+  port: Number(process.env.PORT) || 5000,
 
   headers: {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:122.0) Gecko/20100101 Firefox/122.0',
